62cdcce518a9bd1adf6e0b941cf2853a8a909845
shiyunlian <shiyunlian00@gmail.com>
2022-04-04 16:40:06 -0700

    add properties id, createdAt and corresponding getter and setter methods

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,5 +1,7 @@
 package tacos;
 
+
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
@@ -17,29 +19,64 @@ public class Taco {
 	@NotNull
 	@Size(min=5, message="You must choose at least 1 ingredient")
 	private List<Ingredient> ingredients;
+	
+	private Long id;
+	
+	private Date createdAt = new Date();
+	
 
 	public Taco() {}
-	
-	public Taco(String name, List<Ingredient> ingredients) {
+
+
+	public Taco(@NotNull @Size(min = 5, message = "Name must be at least 5 characters long") String name,
+			@NotNull @Size(min = 5, message = "You must choose at least 1 ingredient") List<Ingredient> ingredients,
+			Long id, Date createdAt) {
+		super();
 		this.name = name;
 		this.ingredients = ingredients;
+		this.id = id;
+		this.createdAt = createdAt;
 	}
+
 
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 
 	public List<Ingredient> getIngredients() {
 		return ingredients;
 	}
 
+
 	public void setIngredients(List<Ingredient> ingredients) {
 		this.ingredients = ingredients;
 	}
-	
 
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
 }
