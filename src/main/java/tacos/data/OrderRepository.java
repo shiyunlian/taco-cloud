4f53e6e3d8aaf3a2cdcef30d9b4d4957d6fd3736
shiyunlian <shiyunlian00@gmail.com>
2022-04-05 21:42:10 -0700

    extend CrudRepository

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,7 +1,13 @@
 package tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
+
 import tacos.TacoOrder;
 
-public interface OrderRepository {
-	TacoOrder save(TacoOrder order);
+//public interface OrderRepository {
+//	TacoOrder save(TacoOrder order);
+//}
+
+public interface OrderRepository extends CrudRepository<TacoOrder, Long>{
+	
 }
